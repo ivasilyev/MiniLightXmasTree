d83ba7723fa2c8e6f90eeb75ea311741f67d0e7a
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-01-05 16:50:47 +0300

    Created file stubs

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git init .
+
+touch .gitignore README.md .ampy deploy.sh config.json __init__.py main.py boot.py config.json deploy.sh
+
+git add init.sh .gitignore README.md .ampy deploy.sh config.json __init__.py main.py boot.py config.json deploy.sh
