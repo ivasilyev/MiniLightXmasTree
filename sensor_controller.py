51c9469e8ce695a9a9c2fcafedf78556e38aed7c
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-01-26 16:35:09 +0300

    Modified sensor control to asynchronous

--- sensor_controller.py
+++ sensor_controller.py
@@ -3,22 +3,37 @@ from utime import sleep
 from onewire import OneWire
 from ds18x20 import DS18X20
 
+try:
+    import uasyncio as asyncio
+except ImportError:
+    import asyncio
+
 
 class SensorController:
-    def __init__(self, pin: int, polling_interval: int = 1):
-        self.temperature_sensor = DS18X20(OneWire(Pin(pin)))
-        self.temperature_chips = self.temperature_sensor.scan()
-        self.pause = polling_interval
+    def __init__(self, pin: int, pause: int = 1):
+        self._pin = pin
+        self._temperature_sensor = DS18X20(OneWire(Pin(pin)))
+        self._temperature_chips = self._temperature_sensor.scan()
+        self.pause = pause
         self.state = dict(temperature=dict())
         print("The sensor controller module started")
 
-    def read_temperature(self):
-        for chip in self.temperature_chips:
-            chip_id = "".join(map(chr, chip))
-            chip_data = self.temperature_sensor.read_temp(chip)
-            self.state["temperature"][chip_id] = chip_data
+    def __repr__(self):
+        return "SensorController object on pin {}".format(self._pin)
+
+    async def read_temperature(self):
+        while True:
+            for chip in self._temperature_chips:
+                chip_id = "".join(map(chr, chip))
+                try:
+                    chip_data = self._temperature_sensor.read_temp(chip)
+                    self.state["temperature"][chip_id] = chip_data
+                except:  # CRC error
+                    continue
+            print(self.state["temperature"])
+            await asyncio.sleep(self.pause)
 
     def run(self):
-        while True:
-            self.read_temperature()
-            sleep(self.pause)
+        loop = asyncio.get_event_loop()
+        _ = loop.create_task(self.read_temperature())
+        loop.run_forever()
